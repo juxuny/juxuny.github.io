07c4b429609ab91a1c709d59158c59b6e5a304f9
juxuny <juxuny@gmail.com>
2021-01-22 00:02:39 +0800

    remove some useless files

--- docs/wenjuanxing/first.php
+++
@@ -1,6 +0,0 @@
-<?php
-$appid='wxcc607f2ac191a91b';
-$redirect_uri = urlencode ( 'https://guanhu.applinzi.com/wenjuanxing/getUserInfo.php' );
-$url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-header("Location:".$url);
-?>
